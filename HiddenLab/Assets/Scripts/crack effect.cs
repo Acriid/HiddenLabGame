0fa3a0b9e5c6eff2ede357db695f193d05ce936d
alexM2232 <2886002@students.wits.ac.za>
2025-05-19 13:53:13 +0200

    the video

---
+++ HiddenLab/Assets/Scripts/crack effect.cs
@@ -0,0 +1,23 @@
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class TriggerByKey : MonoBehaviour
+{
+    public Animator animator;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            animator.SetTrigger("CrackEffect");
+        }
+    }
+   
+    
+}
