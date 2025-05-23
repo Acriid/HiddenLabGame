83fd405cdaa648ce5d53ddb6616eeb4c81b4318e
Acriid <wilrichgerdus@gmail.com>
2025-05-23 09:45:20 +0200

    Added Chasm

--- HiddenLab/Assets/Scripts/Collisions/Chasm Collision.cs
+++ HiddenLab/Assets/Scripts/Collisions/Chasm Collision.cs
@@ -3,27 +3,5 @@ using UnityEngine.Tilemaps;
 
 public class ChasmCollision : MonoBehaviour
 {
-    public Player player;
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        if(player.playerStateMachine.currentPlayerState == player.playerStretchState)
-        {
-            this.GetComponent<TilemapCollider2D>().enabled = false;
-        }
-    }
-    void OnCollisionExit(Collision collision)
-    {
-        if(player.playerStateMachine.currentPlayerState == player.playerStretchState)
-        {
-            this.GetComponent<TilemapCollider2D>().enabled = true;
-        }
-    }
-    void OnTriggerEnter2D(Collider2D collision)
-    {
-        
-    }
-    void OnTriggerExit2D(Collider2D collision)
-    {
 
-    }
 }
