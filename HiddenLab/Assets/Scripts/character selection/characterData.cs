61e4d9f4471aa131b92fc2edb01d969a7e8a6a5a
alexM2232 <2886002@students.wits.ac.za>
2025-06-15 23:30:28 +0200

    character selection

---
+++ HiddenLab/Assets/Scripts/character selection/characterData.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.TextCore.Text;
+
+public class characterData : MonoBehaviour
+{
+    public void SelectCharacter(string characterName)
+    {
+        Debug.Log("Character selected: " + characterName);
+    }
+}
