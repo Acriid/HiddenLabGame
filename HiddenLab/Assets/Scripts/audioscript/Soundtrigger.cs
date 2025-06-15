2c4db2a19775dbe538809f85e1a7ca718b198e04
Erist-Tsire <2803324@students.wits.ac.za>
2025-06-15 15:34:43 +0200

    added more sound effects

--- HiddenLab/Assets/Scripts/audioscript/Soundtrigger.cs
+++ HiddenLab/Assets/Scripts/audioscript/Soundtrigger.cs
@@ -1,12 +1,39 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Soundtrigger : MonoBehaviour
 {
-    void OnTriggerEnter(Collider other)
+    public InputActionReference inputActionReference;
+    bool isintrigger;
+    private void OnEnable()
     {
-        if (other.CompareTag("Player"))
+        inputActionReference.action.performed += OnInputActionReference;
+
+    }
+    private void OnDisable()
+    {
+        inputActionReference.action.performed -= OnInputActionReference;
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player")) 
+        {
+            isintrigger = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isintrigger = false;
+        }
+    }
+    void OnInputActionReference(InputAction.CallbackContext ctx)
+    {
+        if (isintrigger)
         {
             SoundEffectManager.Play("pickup");
+            Debug.Log("Playedsound");
         }
     }
 }
