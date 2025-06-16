cc74b9567fc6a7c8aeff844a7e43132304f0df82
Acriid <wilrichgerdus@gmail.com>
2025-06-16 20:33:36 +0200

    Added proper saves

--- HiddenLab/Assets/Scripts/New Menu/Buttons 1/MainMenu/StartClick.cs
+++ HiddenLab/Assets/Scripts/New Menu/Buttons 1/MainMenu/StartClick.cs
@@ -1,10 +1,19 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class StartClick : MonoBehaviour 
+public class StartClick : MonoBehaviour,iDataPersistence
 {
+    private int ScenetoLoad;
+    public void LoadData(GameData data)
+    {
+        ScenetoLoad = data.CurrentScene;
+    }
+    public void SaveData(ref GameData data)
+    {
+
+    }
     public void OnClick()
     {
-        SceneManager.LoadScene("Level 1");
+        SceneManager.LoadScene(ScenetoLoad);
     }
 }
