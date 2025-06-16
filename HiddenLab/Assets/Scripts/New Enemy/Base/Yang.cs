e1160c2aa12e697d7d13387dbb1bec62c4f1a5d8
Erist-Tsire <2803324@students.wits.ac.za>
2025-06-17 01:02:45 +0200

    lvl2 audio update

--- HiddenLab/Assets/Scripts/New Enemy/Base/Yang.cs
+++ HiddenLab/Assets/Scripts/New Enemy/Base/Yang.cs
@@ -4,15 +4,26 @@ public class Yang : Enemy
 {
     [SerializeField] private PlayerAttributes playerAttributes;
     private bool InLight;
+    private AudioSource audioSource;
+
+    void Awake()
+    {
+        // Get the AudioSource component attached to this GameObject
+        audioSource = GetComponent<AudioSource>();
+    }
+
+
     void OnEnable()
     {
         InLight = playerAttributes.InLight;
         CanSeePlayer = !InLight;
         playerAttributes.OnInLightChange += HandleInLightChange;
+        audioSource.Play();
     }
     void OnDisable()
     {
         playerAttributes.OnInLightChange -= HandleInLightChange;
+        audioSource.Stop();
     }
     void OnDestroy()
     {
--- HiddenLab/Assets/Scripts/New Enemy/Base/Yin.cs
+++ HiddenLab/Assets/Scripts/New Enemy/Base/Yin.cs
@@ -4,15 +4,25 @@ public class Yin : Enemy
 {
     [SerializeField] private PlayerAttributes playerAttributes;
     private bool InLight;
+    private AudioSource audioSource;
+
+    void Awake()
+    {
+        // Get the AudioSource component attached to this GameObject
+        audioSource = GetComponent<AudioSource>();
+    }
+
     void OnEnable()
     {
         InLight = playerAttributes.InLight;
         CanSeePlayer = InLight;
         playerAttributes.OnInLightChange += HandleInLightChange;
+        audioSource.Play();
     }
     void OnDisable()
     {
         playerAttributes.OnInLightChange -= HandleInLightChange;
+        audioSource.Stop();
     }
     void OnDestroy()
     {
